aeb39d1a61cef968adf5345a01b8eaf9b5cccacf
Topech <benlbttm0@gmail.com>
2026-07-03 15:32:01 +0800

    refactor(nixos-bootstrap): use nix-shell for isolated git install

--- bootstraps/nixos-bootstrap.sh
+++ bootstraps/nixos-bootstrap.sh
@@ -4,9 +4,6 @@ set -e  # set bash to exit on fail
 set -x  # set bash to echo each command run
 
 
-nix-env --install git
-
-
 
 BOOTSTRAP_DIR="/tmp/dotfiles-bootstrap"
 
@@ -21,10 +18,8 @@ REPO_BRANCH=master
 DOTFILES_REPO_URL="https://github.com/${REPO_OWNER}/${REPO}"
 
 cd "$BOOTSTRAP_DIR"
-git clone  \
-	--depth 1  \
-	--branch "${REPO_BRANCH}"  \
-	"${DOTFILES_REPO_URL}"
+nix-shell git --run "git clone --depth 1 --branch ${REPO_BRANCH} ${DOTFILES_REPO_URL}"
+
 
 
 echo 'System bootstrapped with temporary dotfiles repo at:'
