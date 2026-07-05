cf581b32c6d3508d3531341d8a38ae0b73d5be8f
Topech <benlbttm0@gmail.com>
2026-07-05 23:46:01 +0800

    fix(nixos): stop lockout on user creation with no pw

--- dotfiles/nixos/hosts/crash-dummy/default.nix
+++ dotfiles/nixos/hosts/crash-dummy/default.nix
@@ -15,6 +15,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.voldwart = {
     isNormalUser = true;
+    initialPassword = "password";
     description = "voldwart";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
