6beea60b75a5adabbc6c9dd241fe39dad33c25af
Topech <benlbttm0@gmail.com>
2026-09-02 16:24:43 +0800

    ollama env vars for better agentic coding

--- dotfiles/nixos/modules/ollama/ollama-vulkan.nix
+++ dotfiles/nixos/modules/ollama/ollama-vulkan.nix
@@ -9,12 +9,15 @@
      OLLAMA_NO_CLOUD = "1";
      
      # model config
-     OLLAMA_CONTEXT_LENGTH = "50000";
-     OLLAMA_KEEP_ALIVE = "30m";
+     OLLAMA_CONTEXT_LENGTH = "40000";
      OLLAMA_NUM_PARALLEL = "1";
+     # OLLAMA_KEEP_ALIVE = "30m";
+     OLLAMA_KEEP_ALIVE = "-1";  # -1 = never unload
+
      
      # speed up with cache
      OLLAMA_KV_CACHE_TYPE="q8_0";
+     # OLLAMA_KV_CACHE_TYPE="f16";
     };
   };
 }
