f994d80f10911091b9bd1540a8007e1cd98fc999
Topech <benlbttm0@gmail.com>
2026-09-08 10:20:02 +0800

    q8 kv cache llama-cpp

--- dotfiles/nixos/modules/llm/llama-cpp/default.nix
+++ dotfiles/nixos/modules/llm/llama-cpp/default.nix
@@ -14,20 +14,23 @@
     # NOTE: NEXT RELEASE: you will config in an ini file.
     modelsPreset =  {
       "*" = {
-        "ctx-size" = 40000;
-        "n-gpu-layers" = -1;
-        "models-max" = 1;
+        cache-type-k = "q8_0";
+        cache-type-v = "q8_0";
+        flash-attn = "true";  # stops degradation using quantised cache
+        ctx-size = 40000;
+        n-gpu-layers = -1;
+        models-max = 1;
       };
 
       "qwen3.8-27b-q3-unsloth" = {
-        "hf-repo" = "unsloth/Qwen3.8-27B-GGUF";
-        "hf-file" = "Qwen3.8-27B-UD-IQ3_XXS.gguf";
+        hf-repo = "unsloth/Qwen3.8-27B-GGUF";
+        hf-file = "Qwen3.8-27B-UD-IQ3_XXS.gguf";
       };
 
       "qwen3.5-35b-a3b-q4" = {
-        "hf-repo" = "Qwen/Qwen3.5-35B-A3B-Instruct-GGUF";
-        "hf-file" = "Qwen3.5-35B-A3B-Instruct-Q4_K_M.gguf";
-        "n-gpu-layers" = 10;  # ~ 25% gpu, 10/40 layers
+        hf-repo = "Qwen/Qwen3.5-35B-A3B-Instruct-GGUF";
+        hf-file = "Qwen3.5-35B-A3B-Instruct-Q4_K_M.gguf";
+        n-gpu-layers = 10;  # ~ 25% gpu, 10/40 layers
       };
     };
 
